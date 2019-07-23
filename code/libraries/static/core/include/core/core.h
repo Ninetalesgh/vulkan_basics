224d0423415c0683b99d1d2900d2590020d184ef
Ninetalesgh <camillolukesch@gmail.com>
2019-07-23 22:38:55 +0200

    dx12 roughs from BLUE

--- code/libraries/static/core/include/core/core.h
+++ code/libraries/static/core/include/core/core.h
@@ -10,14 +10,35 @@ namespace bs::modules
 
 namespace bs::core
 {
-
   class PlatformDesc;
   class IApp;
+
+  class Core
+  {
+    static unique<Core> Create();
+
+    void Init();
+    void RegisterApp(IApp*);
+    void RegisterModule(modules::IModule*); //TODO these go into app
+    void Run();
+
+    //receive window messages
+    //receive delta time 
+
+    void TickUI();
+    
+
+
+    void UnregisterModule(modules::IModule*);
+    void UnregisterApp(IApp*);
+    void End();
+
+  };
+
 
   class ICore
   {
   public:
-    static unique<ICore> Create();
 
     virtual void Init(PlatformDesc*) = 0;
     virtual void RegisterApp(IApp*) = 0;
--- code/libraries/static/core/src/core.cpp
+++ code/libraries/static/core/src/core.cpp
@@ -4,42 +4,7 @@
 
 namespace bs::core
 {
-  class Core : public ICore
-  {
-  public:
-    void Init(PlatformDesc*) override
-    {
-    }
-
-    virtual void RegisterApp(IApp*) override
-    {
-    }
-
-    virtual void UnregisterApp(IApp*) override
-    {
-    }
-
-    void RegisterModule(modules::IModule*) override
-    {
-    }
-
-    void UnregisterModule(modules::IModule*) override
-    {
-    }
-
-    void Run() override
-    {
-    }
-
-    void End() override
-    {
-    }
-
-  private:
-    // Inherited via ICore
-  };
-
-  unique<ICore> ICore::Create()
+  unique<Core> Core::Create()
   {
     return std::make_unique<Core>();
     //return new Core();
